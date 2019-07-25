48e77337b5e2d40d0e3f79ad0b0a20f512380af7
Daniel Naftalovich <dannaftalovich@gmail.com>
2019-07-24 20:59:12 -0700

    A handy reset script

---
+++ reset.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# reset Zotero Connector
+# Remove from Chrome manually
+rm -rf /Users/Nafty/Library/Application Support/Google/Chrome/Profile 2/Extensions/ekhagklcjbdpajgpjgmbionohlpdbjgc
+rm -rf /Users/Nafty/Library/Application Support/Google/Chrome/Profile 2/Local Extension Settings/ekhagklcjbdpajgpjgmbionohlpdbjgc
+# re-Add to Chrome
+
+
+# reset ZoteroGoogleDrive-PDFLinker-Cloud caches
+rm -rf /Users/Nafty/Library/Caches/GoogleZoteroPDFLinker
+rm /Users/Nafty/Downloads/ZoteroGoogleDrive-PDFLinker-Cloud/hashfile
+
+
+# reset Zotero 5.0
+rm -rf ~/Zotero
+rm -rf ~/Library/Caches/Zotero
+rm -rf /Users/Nafty/Library/Application Support/Zotero
+rm -rf /Users/Nafty/Library/Group Containers/UBF8T346G9.Office/User
+rm -rf Content.localized/Startup.localized/Word/Zotero.dotm
+rm -rf /Users/Nafty/Library/Preferences/org.zotero.zotero.plist
+rm -rf /Users/Nafty/Library/Saved Application State/org.zotero.zotero.savedState
+
