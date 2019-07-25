a30a828ab96689c4dd3eb1cda2ce77cd3d02aae1
Daniel Naftalovich <dannaftalovich@gmail.com>
2019-07-24 21:02:51 -0700

    Added printout instructions for manual tasks

--- reset.sh
+++ reset.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # reset Zotero Connector
-# Remove from Chrome manually
+echo "Manually 'Remove from Chrome'"
 rm -rf /Users/Nafty/Library/Application Support/Google/Chrome/Profile 2/Extensions/ekhagklcjbdpajgpjgmbionohlpdbjgc
 rm -rf /Users/Nafty/Library/Application Support/Google/Chrome/Profile 2/Local Extension Settings/ekhagklcjbdpajgpjgmbionohlpdbjgc
-# re-Add to Chrome: https://chrome.google.com/webstore/detail/ekhagklcjbdpajgpjgmbionohlpdbjgc
+echo "Manually re-'Add to Chrome' Zotero Connector at https://chrome.google.com/webstore/detail/ekhagklcjbdpajgpjgmbionohlpdbjgc"
 
 
 # reset ZoteroGoogleDrive-PDFLinker-Cloud caches
@@ -13,6 +13,7 @@ rm /Users/Nafty/Downloads/ZoteroGoogleDrive-PDFLinker-Cloud/hashfile
 
 
 # reset Zotero 5.0
+rm -rf /Applications/Zotero.app
 rm -rf ~/Zotero
 rm -rf ~/Library/Caches/Zotero
 rm -rf /Users/Nafty/Library/Application Support/Zotero
@@ -23,4 +24,4 @@ rm -rf /Users/Nafty/Library/Saved Application State/org.zotero.zotero.savedState
 
 
 # reset Zotero-hhs Google Drive folder
-# manually delete everything
+echo 'Manually delete gdrive Zotero-hhs folder'  # manually delete everything  # TODO: can automate this with pydrive
