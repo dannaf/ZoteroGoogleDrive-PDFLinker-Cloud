80ebe15d4a2a1ac5e6e379bcb134b85361f0fac6
Daniel Naftalovich <dannaftalovich@gmail.com>
2019-07-24 21:05:24 -0700

    Rearranging sections

--- reset.sh
+++ reset.sh
@@ -5,19 +5,7 @@
 ## REMOVE ##
 ############
 
-# reset Zotero Connector
-echo "Manually 'Remove from Chrome'"
-rm -rf /Users/Nafty/Library/Application Support/Google/Chrome/Profile 2/Extensions/ekhagklcjbdpajgpjgmbionohlpdbjgc
-rm -rf /Users/Nafty/Library/Application Support/Google/Chrome/Profile 2/Local Extension Settings/ekhagklcjbdpajgpjgmbionohlpdbjgc
-echo "Manually re-'Add to Chrome' Zotero Connector at https://chrome.google.com/webstore/detail/ekhagklcjbdpajgpjgmbionohlpdbjgc"
-
-
-# reset ZoteroGoogleDrive-PDFLinker-Cloud caches
-rm -rf /Users/Nafty/Library/Caches/GoogleZoteroPDFLinker
-rm /Users/Nafty/Downloads/ZoteroGoogleDrive-PDFLinker-Cloud/hashfile
-
-
-# reset Zotero 5.0
+# reset Zotero desktop client (Zotero 5.0, macOS)
 rm -rf /Applications/Zotero.app
 rm -rf ~/Zotero
 rm -rf ~/Library/Caches/Zotero
@@ -27,6 +15,14 @@ rm -rf Content.localized/Startup.localized/Word/Zotero.dotm
 rm -rf /Users/Nafty/Library/Preferences/org.zotero.zotero.plist
 rm -rf /Users/Nafty/Library/Saved Application State/org.zotero.zotero.savedState
 
+# reset Zotero Connector
+echo "Manually 'Remove from Chrome'"
+rm -rf /Users/Nafty/Library/Application Support/Google/Chrome/Profile 2/Extensions/ekhagklcjbdpajgpjgmbionohlpdbjgc
+rm -rf /Users/Nafty/Library/Application Support/Google/Chrome/Profile 2/Local Extension Settings/ekhagklcjbdpajgpjgmbionohlpdbjgc
+
+# reset ZoteroGoogleDrive-PDFLinker-Cloud caches
+rm -rf /Users/Nafty/Library/Caches/GoogleZoteroPDFLinker
+rm /Users/Nafty/Downloads/ZoteroGoogleDrive-PDFLinker-Cloud/hashfile
 
 # reset Zotero-hhs Google Drive folder
 echo 'Manually delete gdrive Zotero-hhs folder'  # manually delete everything  # TODO: can automate this with pydrive
@@ -36,4 +32,13 @@ echo 'Manually delete gdrive Zotero-hhs folder'  # manually delete everything  #
 ## RE-INSTALL ##
 ################
 
+# Zotero desktop client
+echo 'Install Zotero 5.0 or 4.0'
+
+# Zotero Connector
+echo "Manually re-'Add to Chrome' Zotero Connector at https://chrome.google.com/webstore/detail/ekhagklcjbdpajgpjgmbionohlpdbjgc"
+
+# ZoteroGoogleDrive-PDFLinker-Cloud
+
+# Google Drive Zotero-hhs folder
 
